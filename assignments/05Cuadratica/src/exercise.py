552f1edcbd518a319cf2ae1ad19a0cc541aecfc1
paulinazambrano27 <89042787+paulinazambrano27@users.noreply.github.com>
2021-08-29 19:14:47 -0500

    Update exercise.py

--- assignments/05Cuadratica/src/exercise.py
+++ assignments/05Cuadratica/src/exercise.py
@@ -5,6 +5,23 @@ def main():
     a=int(input("Da el valor de a: "))
     b=int(input("Da el valor de b: "))
     c=int(input("Da el valor de c: "))
+    if a == 0 and b == 0:
+        print("No tiene solucion")
+    elif a == 0 and b != 0:
+        x = -c / b
+        print(x)
+    elif a != 0 and b != 0:
+        d = b * b - 4 * a * c
+        if d < 0:
+            print("Raices complejas")
+        elif d > 0:
+            x1 = (-b + d ** (1 / 2) ) / ( 2 * a)
+            x2 = (-b - d ** (1 / 2) ) / ( 2 * a)
+            print(x1)
+            print(x2)
+        else:
+            x = -b / (2 * a)
+            print(x)
     pass
 
 if __name__ == '__main__':
