784c4b6cb335e459023c307940a3e28b4a2d7004
paulinazambrano27 <89042787+paulinazambrano27@users.noreply.github.com>
2021-08-29 19:46:38 -0500

    Update exercise.py

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -3,6 +3,18 @@ def main():
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
+    if x > y and y > z:
+        print(z , y , x)
+    elif  x > z and z > y:
+        print(y , z , x)
+    elif  y > x and x > z:
+        print(z , x , y)
+    elif  y > z and z > x:
+        print(x , z , y)
+    elif  z > x and x > y:
+        print(y , x , z)
+    elif  z > y and y > x:
+        print(x , y , z)
     
 
 
