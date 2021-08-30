62c43fe62ccc97d927cc664a02549fa0c9a6f046
paulinazambrano27 <89042787+paulinazambrano27@users.noreply.github.com>
2021-08-29 19:04:12 -0500

    Update exercise.py

--- assignments/04Cuadrante/src/exercise.py
+++ assignments/04Cuadrante/src/exercise.py
@@ -1,20 +1,20 @@
 def main():
     # Escribe tu código abajo de esta línea
-    grados = int(input("Introduce los grados: "))
-    if grados < 0 or grados > 360:
-        print("excede")
-    elif grados == 0 or grados == 90 or grados == 180 or grados == 270 or grados == 360:
-        print("eje")
-    elif grados > 0 and grados < 90:
-        print("cuadrante 1")
-    elif grados > 90 and grados < 180:
-        print("cuadrante 2")
-    elif grados > 180 and grados < 270:
-        print("cuadrante 3")
-    elif grados > 270 and grados < 360:
-        print("cuadrante 4")
-    
-    pass
+   grados = int( input("Introduce los grados: "))
+
+   if grados < 0 or grados > 360:
+         print("excede")
+   elif grados == 0 or grados == 90 or grados == 180 or grados == 270 or grados == 360:
+         print("eje")
+   elif grados > 0 and grados < 90:
+          print("cuadrante 1")
+   elif grados > 90 and grados < 180:
+          print("cuadrante 2")
+   elif grados > 180 and grados < 270:
+          print("cuadrante 3")
+   elif grados > 270 and grados < 360:
+          print("cuadrante 4")
+pass
 
 if __name__ == '__main__':
     main()
